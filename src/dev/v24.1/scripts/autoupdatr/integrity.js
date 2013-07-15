654f138e7ebd1b5249209a0bf19bfe6a8ba41ea5
Kevin Zhou <kevzho@users.sourceforge.net>
2013-07-14 18:21:13 -0700

    Fixed auto update bug if resources string was blank
    Signed-off-by: Kevin Zhou <kevzho@users.sourceforge.net>

--- src/dev/v24.1/scripts/autoupdatr/integrity.js
+++ src/dev/v24.1/scripts/autoupdatr/integrity.js
@@ -1,17 +1,28 @@
 var integrity = true;
 if (navigator.onLine || md5) {
-	if (localStorage.resources) {
-		var resources = localStorage.resources.split(";");
-		for (var i = 0; i < resources.length - 1; i++) {
-			if (hex_md5(localStorage[resources[i]]) != manifest.md5[resources[i]]) {
+	try {
+		if (localStorage.resources) {
+			var resources = localStorage.resources.split(";");
+			for (var i = 0; i < resources.length - 1; i++) {
+				if (hex_md5(localStorage[resources[i]]) != manifest.md5[resources[i]]) {
+					integrity = false;
+				}
+			}
+		} else {
+			if (localStorage.length > (localStorage.resources.split(";").length + 1)) {
 				integrity = false;
 			}
 		}
+	} catch (e) {
+		integrity = false;
 	}
 }
 
 if (!integrity) {
-	alert("Files were modified (and/or minor update released). File cache has been cleared. Page will now reload to download files.");
 	localStorage.clear();
+	console.error("Integrity check failed");
+	alert("Files were modified (and/or minor update released). File cache has been cleared. Page will now reload to download files.");
 	location.reload();
+} else {
+	console.log("Integrity check complete, file cache OK");
 }
